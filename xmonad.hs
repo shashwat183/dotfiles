7d5cad05ff733272f923db49487bf5137d6e3f93
shashwat pragya <shashwat183@gmail.com>
2022-07-09 01:48:00 +0100

    Redesign xmobar and additional stuff in xmonad
    Redesign xmobar to be transparent and look more
    like macos(not exactly the same). Remove trayer
    from xmobar and xmonad. Add some additional
    keybindings to xmonad.

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Layout.Gaps
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.SpawnOnce
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 
@@ -17,7 +18,8 @@ myStartupHook = do
   spawnOnce "xrandr -s 1920x1080 &"
   spawnOnce "nitrogen --restore &"
   spawnOnce "picom &"
-  spawnOnce "/usr/bin/trayer --edge top --align right --SetDockType true --SetPartialStrut true --width 10 --transparent true --expand true --distance 20 --height 30 --margin 30 --tint 0x1e1e1e --padding 20 &"
+  -- spawnOnce "/usr/bin/trayer --edge top --align right --SetDockType true --SetPartialStrut true --width 10 --transparent true --expand true --distance 5 --height 30 --margin 30 --tint 0x1e1e1e --padding 20 &"
+  setWMName "LG3D"
 
 
 -- main function
@@ -25,6 +27,7 @@ main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
 -- Command to launch the bar.
 myBar = "xmobar"
+-- myBar = "polybar --reload"
 
 -- Custom PP
 myPP = xmobarPP { 
@@ -33,22 +36,38 @@ myPP = xmobarPP {
   , ppExtras = [windowCount]
   }
 
+-- Window swallowing event hook
+-- Introduced in xmonad-contrib 0.17
+-- windowSwallowEventHook = swallowEventHook (className =? "Alacritty" <||> className =? "Termite") (return True)
+
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Main Configuraton, override the defaults to your liking
 myConfig = def
-	{ terminal = "Alacritty"
+	{ terminal = "alacritty"
   , borderWidth = 0
-	, layoutHook = spacingWithEdge 15 $ myLayoutHook
+	, layoutHook = spacingWithEdge 10 $ myLayoutHook
   , startupHook = myStartupHook
   , handleEventHook = fullscreenEventHook
+  , modMask = mod4Mask -- command key
 	}
   `additionalKeysP`
   [
     -- ("M-p", spawn "rofi -modi drun -show")
     ("M-p", spawn "launcher_colorful")
   , ("M-S-p", spawn "rofi -show run")
+  , ("M-S-b", spawn "brave")
+  , ("M-S-n", spawn "notion-app")
+  , ("M-u", spawn "brave --profile-directory=Default --app-id=edcmabgkbicempmpgmniellhbjopafjh")
+  , ("M-S-s", spawn "spotify")
+  , ("M-S-m", spawn "alacritty -e neomutt")
+  , ("M-s", spawn "alacritty -e spt")
+  , ("M-S-t", spawn "alacritty -e tmux")
+  , ("M-S-g", spawn "xdg-open https://www.google.com")
+  , ("M-S-y", spawn "xdg-open https://www.youtube.com")
+  , ("M-t", spawn "xdg-open https://www.twitter.com")
+  , ("M-S-j", spawn "intellij-idea-community")
   ]
 
 myLayoutHook = tiled ||| Mirror tiled ||| Full
